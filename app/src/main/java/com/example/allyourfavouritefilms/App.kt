c106555b83bac3ba04703a9fe7b9bc4e7dce44a4
Tatyana Radyukhina <t.a.radyukhina@yandex.ru>
2022-07-24 15:01:27 +0300

    37.7_Dagger_2_not done

--- app/src/main/java/com/example/allyourfavouritefilms/App.kt
+++ app/src/main/java/com/example/allyourfavouritefilms/App.kt
@@ -1,81 +1,31 @@
 package com.example.allyourfavouritefilms
 
 import android.app.Application
-import com.example.allyourfavouritefilms.DI.AppComponent
-import com.example.allyourfavouritefilms.DI.DaggerAppComponent
+import com.example.allyourfavouritefilms.di.AppComponent
+import com.example.allyourfavouritefilms.di.modules.DatabaseModule
+import com.example.allyourfavouritefilms.di.modules.DomainModule
+import com.example.allyourfavouritefilms.di.modules.RemoteModule
 
-import com.example.allyourfavouritefilms.data.ApiConstants
-import com.example.allyourfavouritefilms.data.MainRepository
-import com.example.allyourfavouritefilms.data.TmdbApi
-import com.example.allyourfavouritefilms.domain.Interactor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 class App : Application() {
-    /*lateinit var repo: MainRepository
-    lateinit var interactor: Interactor
-    lateinit var retrofitService: TmdbApi
-
-    override fun onCreate() {
-        super.onCreate()
-        //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
-        instance = this
-        //Инициализируем репозиторий
-        repo = MainRepository()
-
-        //Создаём кастомный клиент
-        val okHttpClient = OkHttpClient.Builder()
-            //Настраиваем таймауты для медленного интернета
-            .callTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            //Добавляем логгер
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BASIC
-                }
-            })
-            .build()
-
-        //Создаем Ретрофит
-        val retrofit = Retrofit.Builder()
-            //Указываем базовый URL из констант
-            .baseUrl(ApiConstants.BASE_URL)
-            //Добавляем конвертер
-            .addConverterFactory(GsonConverterFactory.create())
-            //Добавляем кастомный клиент
-            .client(okHttpClient)
-            .build()
-//Создаем сам сервис с методами для запросов
-        retrofitService = retrofit.create(TmdbApi::class.java)
-//Инициализируем интерактор
-        interactor = Interactor(repo, retrofitService)
-
-    }
-
-
-    companion object {
-        //Здесь статически хранится ссылка на экземпляр App
-        lateinit var instance: App
-            //Приватный сеттер, чтобы нельзя было в эту переменную присвоить что-либо другое
-            private set
-
-    }*/
     lateinit var dagger: AppComponent
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         //Создаем компонент
-        dagger = DaggerAppComponent.create()
-    }
+        dagger = DaggerAppComponent.builder()
+            .remoteModule(RemoteModule())
+            .databaseModule(DatabaseModule())
+            .domainModule(DomainModule(this))
+            .build()
 
-    companion object {
-        lateinit var instance: App
-            private set
     }
+        companion object {
+            lateinit var instance: App
+                private set
+        }
+
 }
 
 
