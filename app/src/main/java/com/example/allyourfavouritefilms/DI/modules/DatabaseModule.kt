9461083532ec54259147d605cec563bdaa1b6d54
Tatyana Radyukhina <t.a.radyukhina@yandex.ru>
2022-07-26 22:38:35 +0300

    39.SQLite

--- app/src/main/java/com/example/allyourfavouritefilms/DI/modules/DatabaseModule.kt
+++ app/src/main/java/com/example/allyourfavouritefilms/DI/modules/DatabaseModule.kt
@@ -1,13 +1,20 @@
 package com.example.allyourfavouritefilms.DI.modules
 
+import android.content.Context
 import com.example.allyourfavouritefilms.data.MainRepository
+import com.example.allyourfavouritefilms.db.DatabaseHelper
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabaseHelper(context: Context) = DatabaseHelper(context)
+
     @Provides
     @Singleton
-    fun provideRepository() = MainRepository()
+    fun provideRepository(databaseHelper: DatabaseHelper) = MainRepository(databaseHelper)
 }
